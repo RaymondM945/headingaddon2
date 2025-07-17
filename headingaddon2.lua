1b7dcd35314a1821c3508c979666b009f997ad12
RaymondM945 <raymondm9820@gmail.com>
2025-07-17 19:14:41 -0400

    new toggle

--- headingaddon2.lua
+++ headingaddon2.lua
@@ -44,6 +44,9 @@ local checkforheal = false
 local testheal = "Test Main"
 local followtarget = nil
 local combatmode = false
+local Otherhealertarget = nil
+local lastHealedTarget = nil
+local healthreshold = 90
 f:SetScript("OnUpdate", function(self, elapsed)
 	if IsInGroup() then
 		if not checkforheal then
@@ -81,16 +84,18 @@ f:SetScript("OnUpdate", function(self, elapsed)
 				local unit = (i == 0) and "player" or "party" .. i
 
 				if UnitExists(unit) then
-					local name = UnitName(unit)
-					local health = UnitHealth(unit)
-					local maxHealth = UnitHealthMax(unit)
-					local hpPercent = (maxHealth > 0) and (health / maxHealth) * 100 or 0
+					if UnitName(unit) ~= Otherhealertarget then
+						local name = UnitName(unit)
+						local health = UnitHealth(unit)
+						local maxHealth = UnitHealthMax(unit)
+						local hpPercent = (maxHealth > 0) and (health / maxHealth) * 100 or 0
 
-					print(name .. " - " .. hpPercent)
+						-- print(name .. " - " .. hpPercent)
 
-					if hpPercent < lowesthp then
-						lowesthp = hpPercent
-						lowestunitname = unit
+						if hpPercent < lowesthp then
+							lowesthp = hpPercent
+							lowestunitname = unit
+						end
 					end
 				end
 			end
@@ -100,23 +105,28 @@ f:SetScript("OnUpdate", function(self, elapsed)
 				local currentHP = UnitHealth(petUnit)
 				local maxHP = UnitHealthMax(petUnit)
 				local petPercent = (maxHP > 0) and (currentHP / maxHP) * 100 or 0
-				print(petName .. " - " .. petPercent)
+				-- print(petName .. " - " .. petPercent)
 
-				if petPercent < lowesthp then
-					lowesthp = petPercent
-					lowestunitname = petUnit
+				if petName ~= Otherhealertarget then
+					if petPercent < lowesthp then
+						lowesthp = petPercent
+						lowestunitname = petUnit
+					end
 				end
 			end
 
-			print("lowest Hp in the group is " .. lowestunitname .. " With hp: " .. lowesthp)
+			-- print("lowest Hp in the group is " .. lowestunitname .. " With hp: " .. lowesthp)
 			local mana = UnitPower("player", 0)
 
-			if mana >= 0 and lowesthp < 85 then
+			if mana >= 0 and lowesthp < healthreshold then
 				local spellName = UnitCastingInfo("player")
 				local usable, nomana = IsUsableSpell("Holy Light")
 				if lowesthp <= selectedpercentage then
 					local r, g, b = unpack(colorMap[lowestunitname] or { 0, 0, 0, 1 })
 					if spellName ~= "Holy Light" and usable then
+						if lastHealedTarget ~= lowestunitname then
+							lastHealedTarget = lowestunitname
+						end
 						box.texture:SetColorTexture(r, g, b, 1)
 					else
 						box.texture:SetColorTexture(0, 0, 0, 1)
@@ -137,6 +147,7 @@ f:SetScript("OnUpdate", function(self, elapsed)
 			end
 		end
 	end
+	-- print("other healer target:", Otherhealertarget)
 end)
 
 local colorMapOrder = {
@@ -311,16 +322,101 @@ UIDropDownMenu_SetButtonWidth(dropdown3, 150)
 UIDropDownMenu_SetSelectedValue(dropdown3, testheal)
 UIDropDownMenu_JustifyText(dropdown3, "LEFT")
 
+-- Create the fourth dropdown
+local dropdown4 = CreateFrame("Frame", "MyFourthDropdown", UIParent, "UIDropDownMenuTemplate")
+dropdown4:SetPoint("TOPLEFT", dropdown3, "BOTTOMLEFT", 0, -50)
+
+-- Label for the fourth dropdown
+local dropdown4Label = dropdown4:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+dropdown4Label:SetPoint("BOTTOMLEFT", dropdown4, "TOPLEFT", 20, 0)
+dropdown4Label:SetText("Begin healing at")
+
+-- Dropdown options
+local additionalOptions = { 90, 85, 80, 75, 70, 65, 60 }
+
+UIDropDownMenu_Initialize(dropdown4, function(self, level)
+	for _, option in ipairs(additionalOptions) do
+		local info = UIDropDownMenu_CreateInfo()
+		info.text = option
+		info.value = option
+		info.func = function()
+			healthreshold = option
+			UIDropDownMenu_SetSelectedValue(dropdown4, option)
+			print("healing threshold selected:", option)
+			-- Add any additional logic you want here
+		end
+		UIDropDownMenu_AddButton(info, level)
+	end
+end)
+
+UIDropDownMenu_SetWidth(dropdown4, 150)
+UIDropDownMenu_SetButtonWidth(dropdown4, 150)
+UIDropDownMenu_SetSelectedValue(dropdown4, healthreshold)
+UIDropDownMenu_JustifyText(dropdown4, "LEFT")
+
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("AUTOFOLLOW_BEGIN")
 frame:RegisterEvent("AUTOFOLLOW_END")
-frame:SetScript("OnEvent", function(self, event, arg1)
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:RegisterEvent("UNIT_SPELLCAST_START")
+frame:RegisterEvent("UNIT_SPELLCAST_STOP")
+
+-- C_ChatInfo.RegisterAddonMessagePrefix("healingtarget")
+-- C_ChatInfo.RegisterAddonMessagePrefix("doneheal")
+local currentCast = nil
+frame:SetScript("OnEvent", function(self, event, ...)
 	if event == "AUTOFOLLOW_BEGIN" then
-		print("Started following:", arg1)
-		followtarget = arg1
+		local target = ...
+		print("Started following:", target)
+		followtarget = target
 	elseif event == "AUTOFOLLOW_END" then
 		print("Stopped following")
 		followtarget = nil
+	elseif event == "CHAT_MSG_ADDON" then
+		local prefix, message, channel, sender = ...
+		-- print("sender", sender)
+		-- if sender == UnitName("player") then
+		-- 	return
+		-- end
+
+		-- if prefix == "healingtarget" then
+		-- 	print("healing the target:", message, "from", sender)
+		-- 	Otherhealertarget = message
+		-- elseif prefix == "doneheal" then
+		-- 	print("done healing the target:", message, "from", sender)
+		-- 	Otherhealertarget = nil
+		-- end
+	elseif event == "UNIT_SPELLCAST_START" then
+		local unit = ...
+
+		currentCast = UnitCastingInfo(unit)
+
+		local name = UnitName(unit)
+
+		if unit ~= "player" and (currentCast == "Holy Light" or currentCast == "Flash of Light") then
+			Otherhealertarget = UnitName(unit .. "target")
+			print(unit, ":", name, " is casting ", currentCast, " on ", Otherhealertarget)
+		end
+
+		-- if unit == "player" then
+		-- 	currentCast = UnitCastingInfo("player")
+		-- 	C_ChatInfo.SendAddonMessage("healingtarget", "party1", "PARTY")
+		-- 	if lastHealedTarget then
+		-- 		C_ChatInfo.SendAddonMessage("healingtarget", lastHealedTarget, "PARTY")
+		-- 	end
+		-- end
+	elseif event == "UNIT_SPELLCAST_STOP" then
+		local unit = ...
+		local name = UnitName(unit)
+		if unit ~= "player" and (currentCast == "Holy Light" or currentCast == "Flash of Light") then
+			print(unit, ":", name, " is stopped casting  ", currentCast)
+			Otherhealertarget = nil
+		end
+
+		-- if unit == "player" and currentCast == "Holy Light" then
+		-- 	C_ChatInfo.SendAddonMessage("doneheal", "party1", "PARTY")
+		-- end
+		-- currentCast = nil
 	end
 end)
 
@@ -341,4 +437,49 @@ checkbox:SetScript("OnClick", function(self)
 	combatmode = self:GetChecked()
 	print("Checkbox is", combatmode and "checked" or "unchecked")
 	-- Store checked value or update behavior
+end)
+
+local toggleButton = CreateFrame("Button", "MyToggleUIButton", UIParent, "UIPanelButtonTemplate")
+toggleButton:SetSize(100, 25) -- width, height
+toggleButton:SetPoint("TOP", UIParent, "TOP", 250, -10)
+toggleButton:SetText("Toggle UI")
+
+local uiVisible = true
+
+local function ToggleUI()
+	uiVisible = not uiVisible
+
+	if uiVisible then
+		MyColorDropdown:Show()
+		MySecondDropdown:Show()
+		MyThirdDropdown:Show()
+		MyFourthDropdown:Show()
+		MyCheckbox:Show()
+		MyAddonCheckbox:Show()
+	else
+		MyColorDropdown:Hide()
+		MySecondDropdown:Hide()
+		MyThirdDropdown:Hide()
+		MyFourthDropdown:Hide()
+		MyCheckbox:Hide()
+		MyAddonCheckbox:Hide()
+	end
+end
+
+-- Bind function to button
+toggleButton:SetScript("OnClick", ToggleUI)
+
+local toggleBoxButton = CreateFrame("Button", "MyToggleBoxButton", UIParent, "UIPanelButtonTemplate")
+toggleBoxButton:SetSize(100, 25)
+toggleBoxButton:SetPoint("TOP", toggleButton, "BOTTOM", 0, -5) -- just below the toggle UI button
+toggleBoxButton:SetText("Toggle Box")
+local boxVisible = true
+
+toggleBoxButton:SetScript("OnClick", function()
+	boxVisible = not boxVisible
+	if boxVisible then
+		box:Show()
+	else
+		box:Hide()
+	end
 end)
